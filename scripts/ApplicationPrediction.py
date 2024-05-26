41511e4acdb2a7d7b511a3a23adfb464f76b5a85
felipealsoares <falsoares@sga.pucminas.br>
2024-05-26 20:44:29 -0300

    Alterações

--- scripts/ApplicationPrediction.py
+++ scripts/ApplicationPrediction.py
@@ -1,11 +1,7 @@
 
 
 
-# from processing import ControllerProcessing
-# from processing import _ControllerProcessingCHANGED as ControllerProcessing
-# from processing import __ControllerProcessingCHANGEDCopy as ControllerProcessing
-# from processing import __ControllerProcessingCHANGEDNewMetrics as ControllerProcessing
-from processing import _ControllerProcessingCHANGEDCaseStudyHasheemClassification as ControllerProcessing
+from processing import ControllerProcessing
 
 if __name__ == "__main__":
     ControllerProcessing.main()
